d6abeca5b8dccf4a9408fbaac988d358d9b904ce
q <andreyb0033@gmail.com>
2025-12-26 13:09:05 +0400

    init commit

---
+++ src/test/java/com/finance/finance_app/FinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.finance_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
